dce92744108a996ade50260b0c31a3fea8846f03
Igor Clark <igor@Igor-Clarks-iMac.local>
2012-02-20 16:17:24 -0800

    changed example url

--- ncsa_combined_log_parser.erl
+++ ncsa_combined_log_parser.erl
@@ -1,5 +1,5 @@
 % reads in NCSA combined-format log lines like this:
-% 63.84.95.12 - - [06/Feb/2012:04:17:29 +0000] "GET /comments/recent HTTP/1.1" 200 1 "http://www.cokepolarbowl.com/comments/" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.77 Safari/535.7"
+% 63.84.95.12 - - [06/Feb/2012:04:17:29 +0000] "GET /comments/recent HTTP/1.1" 200 1 "http://www.apple.com/" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.77 Safari/535.7"
 %
 -module(ncsa_combined_log_parser).
 -export([start/2]).
